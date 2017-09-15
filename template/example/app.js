a86c05373efba747a2498cd063689900a5b01c3e
陈荣 <cr1@strongsoft.net>
2017-09-15 11:01:16 +0800

    提交bug

--- template/example/app.js
+++ template/example/app.js
@@ -1,8 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import { TestApp } from '../dist/vueplugintest'
-import '../dist/vueplugintest.css'
+import { TestApp } from '../dist/{{ name }}'
+import '../dist/{{ name }}.css'
 
 Vue.config.productionTip = false
 
